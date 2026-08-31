28ae0f66eba8b748a873421e079be0174c1dd0d4
Reddy <redrandomcoreguy@gmail.com>
2026-08-31 08:55:23 -0500

    launch.sh
    added a battery detection system

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -2,4 +2,8 @@
 
 killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+export DEFAULT_BATTERY=$(ls -1 /sys/class/power_supply/ 2>/dev/null | grep -E '^BAT' | head -n 1)
+export DEFAULT_ADAPTER=$(ls -1 /sys/class/power_supply/ 2>/dev/null | grep -E '^(AC|ADP)' | head -n 1)
+
 polybar example 2>&1 | tee -a /tmp/polybar.log & disown
